3d8f444ba35f0eec14e8f4724d224ff26b09be9f
neodevelop <juan@makingdevs.com>
2019-06-15 10:10:15 -0500

    Primer commit, comienzo CodeBReaker

---
+++ src/main/java/com/makingdevs/codebreaker/CodeBreaker/CodeBreakerApplication.java
@@ -0,0 +1,13 @@
+package com.makingdevs.codebreaker.CodeBreaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBreakerApplication.class, args);
+	}
+
+}
