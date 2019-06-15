4c7d9ae8ce68991ad56a0d0c2a3c2562e1092099
GabrielMorenoGarrido <gabomgar@gmail.com>
2019-06-15 12:43:52 -0500

    Agregando metodo para calcular numberGuess

--- src/main/java/com/makingdevs/codebreaker/services/GameService.java
+++ src/main/java/com/makingdevs/codebreaker/services/GameService.java
@@ -1,24 +1,50 @@
 package com.makingdevs.codebreaker.services;
+
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
 import com.makingdevs.codebreaker.model.CodeBreaker;
 
 @Component
 public class GameService {
 
-  public CodeBreaker initGameWith(Integer number){
+  public CodeBreaker initGameWith(Integer number) {
     return new CodeBreaker(number);
   }
 
-  public String guess(CodeBreaker game, int guessNumber){
+  public String guess(CodeBreaker game, int guessNumber) {
     int secret = game.getSecret();
-    if (guessNumber == 4701){
-      return "__";
-    }else if(guessNumber == 1538){
-      return "***";
-    }else{
-    return "*";
+
+    String result = calculateGuess(secret, guessNumber);
+
+    return result;
+
+  }
+
+  public String calculateGuess(int numberSecret, int number) {
+
+    String result = "";
+    String sNumber = String.valueOf(number);
+    String sSecret = String.valueOf(numberSecret);
+    int position = 0;
+    int positionS = 0;
+
+    for (char n : sNumber.toCharArray()) {
+        for (char s : sSecret.toCharArray()) {
+          if(n == s && position == positionS) {
+            result += "_";
+          } else if (n == s && position != positionS) {
+            result += "*";
+          } else {
+            result += "";
+          }
+          positionS ++;
+        }
+        position ++;
     }
 
+    return result;
   }
 
 }
