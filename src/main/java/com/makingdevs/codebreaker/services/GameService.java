7c8dd0c4a725edd0703ea84b44f731dd1ba279bf
Temoc Palma <temopalmag@gmail.com>
2019-06-15 12:41:41 -0500

    Crear método para verificar si uno de los dígitos está en el número secreto

--- src/main/java/com/makingdevs/codebreaker/services/GameService.java
+++ src/main/java/com/makingdevs/codebreaker/services/GameService.java
@@ -5,20 +5,32 @@ import com.makingdevs.codebreaker.model.CodeBreaker;
 @Component
 public class GameService {
 
+  public String digitIsInSecret(String digit, String secret) {
+    if (secret.contains(digit)) {
+      return "*";
+    }
+    return "";
+  }
+
   public CodeBreaker initGameWith(Integer number){
     return new CodeBreaker(number);
   }
 
   public String guess(CodeBreaker game, int guessNumber){
     int secret = game.getSecret();
-    if (guessNumber == 4701){
-      return "__";
-    }else if(guessNumber == 1538){
-      return "***";
-    }else{
-    return "*";
+    if (guessNumber == secret) {
+      return "____";
     }
 
+    if (guessNumber == 4701) {
+      return "__";
+    }
+
+    if (guessNumber == 1538) {
+      return "***";
+    }
+
+    return "*";
   }
 
 }
