99c9a8db8808ab9c9f51d3366fc9bb74cf4d454e
Rafael Avelar <rafaelksd@live.com>
2019-06-15 12:38:39 -0500

    DEsarrollo de la primera prueba.

--- src/main/java/com/makingdevs/codebreaker/services/GameService.java
+++ src/main/java/com/makingdevs/codebreaker/services/GameService.java
@@ -1,24 +1,40 @@
 package com.makingdevs.codebreaker.services;
+
 import org.springframework.stereotype.Component;
 import com.makingdevs.codebreaker.model.CodeBreaker;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class GameService {
 
-  public CodeBreaker initGameWith(Integer number){
-    return new CodeBreaker(number);
-  }
-
-  public String guess(CodeBreaker game, int guessNumber){
-    int secret = game.getSecret();
-    if (guessNumber == 4701){
-      return "__";
-    }else if(guessNumber == 1538){
-      return "***";
-    }else{
-    return "*";
+    public CodeBreaker initGameWith(Integer number) {
+        return new CodeBreaker(number);
     }
 
-  }
+    public String guess(CodeBreaker game, int guessNumber) {
 
+        String resp = "";
+
+        String secret = String.valueOf(game.getSecret());
+        String strGuessNumber = String.valueOf(guessNumber);
+
+        List<Character> secretsCharacterList = new ArrayList<>();
+        List<Character> guessCharacterList = new ArrayList<>();
+
+        for (int x = 0; x < secret.length(); x++) {
+            secretsCharacterList.add(secret.charAt(x));
+        }
+
+        for (int x = 0; x < strGuessNumber.length(); x++) {
+            for (Character c : secretsCharacterList) {
+                if (strGuessNumber.charAt(x) == c) {
+                    resp += "_";
+                }
+            }
+        }
+
+        return resp;
+    }
 }
