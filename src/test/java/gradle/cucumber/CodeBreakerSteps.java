e8e48837f951806bfa6eb3bfe9f912911cbfcb61
neodevelop <juan@makingdevs.com>
2019-06-29 10:33:07 -0500

    Agrego feature para cucumber y steps

---
+++ src/test/java/gradle/cucumber/CodeBreakerSteps.java
@@ -0,0 +1,23 @@
+package gradle.cucumber;
+
+import cucumber.api.java.en.*;
+
+class CodeBreakerSteps {
+    @Given("a mistery number {int}")
+    public void a_mistery_number(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("we start the game")
+    public void we_start_the_game() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("the game must be initialized")
+    public void the_game_must_be_initialized() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+}
