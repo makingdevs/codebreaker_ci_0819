d0f1a8a6bf6cb5e907cb4b0204eba00eab71539f
Temoc Palma <temopalmag@gmail.com>
2019-06-29 10:10:52 -0500

    Ajustar método guess

--- src/main/java/com/makingdevs/codebreaker/services/GameService.java
+++ src/main/java/com/makingdevs/codebreaker/services/GameService.java
@@ -1,15 +1,41 @@
 package com.makingdevs.codebreaker.services;
 import org.springframework.stereotype.Component;
 import com.makingdevs.codebreaker.model.CodeBreaker;
+import java.util.*;
 
 @Component
 public class GameService {
 
-  public String digitIsInSecret(String digit, String secret) {
-    if (secret.contains(digit)) {
+  public String digitIsInSecret(String digit, int secret) {
+    String secretString = Integer.toString(secret);
+    if (secretString.contains(digit)) {
       return "*";
     }
     return "";
+  }
+
+  public String[] getDigitList(int number) {
+    String numberString = Integer.toString(number);
+    return numberString.split("");
+  }
+
+  public String processGuessNumber(int guess, int secret) {
+    String[] guessArray = getDigitList(guess);
+    String[] secretArray = getDigitList(secret);
+    List<String> inPosition = new ArrayList<String>();
+    List<String> inNoPosition = new ArrayList<String>();
+    for (int i = 0; i < 4; i++) {
+      String digitGuess = guessArray[i];
+      if (digitGuess.equals(secretArray[i])) {
+        inPosition.add("_");
+      } else {
+        inNoPosition.add(digitIsInSecret(digitGuess, secret));
+      }
+    }
+
+    String resultInPosition = String.join("", inPosition);
+    String resultInNoPosition = String.join("", inNoPosition);
+    return resultInPosition.concat(resultInNoPosition);
   }
 
   public CodeBreaker initGameWith(Integer number){
@@ -21,16 +47,7 @@ public class GameService {
     if (guessNumber == secret) {
       return "____";
     }
-
-    if (guessNumber == 4701) {
-      return "__";
-    }
-
-    if (guessNumber == 1538) {
-      return "***";
-    }
-
-    return "*";
+    return processGuessNumber(guessNumber, secret);
   }
-
+  
 }
