b8cc550faa348f09642ee821c7087769f8b22317
neodevelop <juan@makingdevs.com>
2019-06-29 11:06:43 -0500

    Configuro Geb

--- src/test/java/gradle/cucumber/CodeBreakerSteps.java
+++ src/test/java/gradle/cucumber/CodeBreakerSteps.java
@@ -6,15 +6,20 @@ import com.makingdevs.codebreaker.services.GameService;
 import geb.junit4.GebTest;
 
 import cucumber.api.java.en.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class CodeBreakerSteps extends GebTest {
 
     private GameService gameService = new GameService();
 
     @Given("a mistery number {int}")
     public void a_mistery_number(Integer number) {
-        CodeBreaker game = gameService.initGameWith(4351);
-		assert(game.getSecret() == 4351);
+        browser.go("/");
     }
 
     @When("we start the game")
---
+++ src/test/resources/GebConfig.groovy
@@ -0,0 +1,17 @@
+import org.openqa.selenium.firefox.FirefoxDriver
+
+waiting {
+	timeout = 2
+}
+
+environments {
+
+	firefox {
+		atCheckWaiting = 1
+
+		driver = { new FirefoxDriver() }
+	}
+
+}
+
+baseUrl = "http://localhost:8080"
